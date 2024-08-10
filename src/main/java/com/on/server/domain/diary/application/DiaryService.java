fa102cf31f1757f8c64564e3bfe5286aced16d2a
ri-naa <rina288@ewhain.net>
2024-08-10 18:26:24 +0900

    feat: 일기 홈 조회하기

--- src/main/java/com/on/server/domain/diary/application/DiaryService.java
+++ src/main/java/com/on/server/domain/diary/application/DiaryService.java
@@ -1,12 +1,19 @@
 package com.on.server.domain.diary.application;
 
+import com.on.server.domain.diary.domain.Diary;
 import com.on.server.domain.diary.domain.repository.DiaryRepository;
+import com.on.server.domain.diary.dto.DiaryDto;
 import com.on.server.domain.diary.dto.DiaryRequestDto;
 import com.on.server.domain.diary.dto.DiaryResponseDto;
 import com.on.server.domain.user.domain.User;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,9 +25,30 @@ public class DiaryService {
     // 1. 일기 홈 조회하기
     public DiaryResponseDto getDiary(User user) {
 
-        return null;
+        //user 일기 목록 찾기
+        List<Diary> diaryList = diaryRepository.findByUser(user);
+
+        //d-day 계산하기
+        Long dDayNow = ChronoUnit.DAYS.between(user.getStartDate(), LocalDate.now());
+
+        //diaryDto List 만들기
+        List<DiaryDto> diaryDtoList = getDiaryDto(diaryList);
+
+        //date List 만들기
+        List<LocalDate> dateList = diaryDtoList.stream()
+                .map(DiaryDto::getWrittenDate)
+                .collect(Collectors.toList());
+
+        return new DiaryResponseDto(user.getUniversity().getName(), dDayNow, diaryDtoList, dateList);
     }
 
+    private static List<DiaryDto> getDiaryDto(List<Diary> diaryList) {
+        return diaryList.stream()
+                .map(diary -> new DiaryDto(
+                        diary.getWrittenDate(),
+                        diary.getContent(),
+                        diary.getDDay())).toList();
+    }
 
     // 2. 일기 등록하기
     public void createDiary(User user, DiaryRequestDto diaryRequestDto) {
