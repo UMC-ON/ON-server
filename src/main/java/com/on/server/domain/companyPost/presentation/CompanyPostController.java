05bf2ba02955ba8eb51e398639c59e2176d3a899
isuHan <gkswltn3736@gmail.com>
2024-08-12 03:02:54 +0900

    feat: user 커스텀 어노테이션 추가

--- src/main/java/com/on/server/domain/companyPost/presentation/CompanyPostController.java
+++ src/main/java/com/on/server/domain/companyPost/presentation/CompanyPostController.java
@@ -8,6 +8,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +25,10 @@ public class CompanyPostController {
 
     // 1. 모든 게시글 조회
     @Operation(summary = "모든 동행구하기 게시글 조회")
+    @PreAuthorize("@securityService.isActiveAndNotNoneUser() and hasAnyRole('ACTIVE', 'AWAIT', 'TEMPORARY')")
     @GetMapping
-    public ResponseEntity<List<CompanyPostResponseDTO>> getAllCompanyPosts() {
+    public ResponseEntity<List<CompanyPostResponseDTO>> getAllCompanyPosts(@AuthenticationPrincipal UserDetails userDetails
+    ) {
         List<CompanyPostResponseDTO> posts = companyPostService.getAllCompanyPosts();
         return ResponseEntity.ok(posts);
     }
@@ -31,32 +36,36 @@ public class CompanyPostController {
 
     // 2. 특정 게시글 조회
     @Operation(summary = "특정 동행구하기 게시글 조회")
+    @PreAuthorize("@securityService.isActiveAndNotNoneUser() and hasAnyRole('ACTIVE', 'AWAIT', 'TEMPORARY')")
     @GetMapping("/{companyPostId}")
-    public ResponseEntity<CompanyPostResponseDTO> getCompanyPostById(@PathVariable Long companyPostId) {
+    public ResponseEntity<CompanyPostResponseDTO> getCompanyPostById(@PathVariable Long companyPostId, @AuthenticationPrincipal UserDetails userDetails) {
         CompanyPostResponseDTO post = companyPostService.getCompanyPostById(companyPostId);
         return ResponseEntity.ok(post);
     }
 
     // 3. 새로운 게시글 작성
     @Operation(summary = "새로운 동행구하기 게시글 작성")
+    @PreAuthorize("@securityService.isActiveAndNotNoneUser() and hasAnyRole('ACTIVE')")
     @PostMapping
-    public ResponseEntity<CompanyPostResponseDTO> createCompanyPost(@RequestBody CompanyPostRequestDTO requestDTO) {
+    public ResponseEntity<CompanyPostResponseDTO> createCompanyPost(@RequestBody CompanyPostRequestDTO requestDTO, @AuthenticationPrincipal UserDetails userDetails) {
         CompanyPostResponseDTO createdPost = companyPostService.createCompanyPost(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
     }
 
     // 4. 마이페이지에서 자기가 작성한 모든 게시글 조회
     @Operation(summary = "자기가 작성한 모든 동행구하기 게시글 모아보기(조회)")
+    @PreAuthorize("@securityService.isActiveAndNotNoneUser() and hasAnyRole('ACTIVE', 'AWAIT', 'TEMPORARY')")
     @GetMapping("/user/{userId}")
-    public ResponseEntity<List<CompanyPostResponseDTO>> getCompanyPostsByUserId(@PathVariable Long userId) {
+    public ResponseEntity<List<CompanyPostResponseDTO>> getCompanyPostsByUserId(@PathVariable Long userId, @AuthenticationPrincipal UserDetails userDetails) {
         List<CompanyPostResponseDTO> posts = companyPostService.getCompanyPostsByUserId(userId);
         return ResponseEntity.ok(posts);
     }
 
     // 5. 마이페이지에서 자기가 작성한 특정 게시글 삭제
     @Operation(summary = "자기가 작성한 특정 동행구하기 게시글 삭제")
+    @PreAuthorize("@securityService.isActiveAndNotNoneUser() and hasAnyRole('ACTIVE')")
     @DeleteMapping("/user/{userId}/{companyPostId}")
-    public ResponseEntity<Void> deleteCompanyPost(@PathVariable Long userId, @PathVariable Long companyPostId) {
+    public ResponseEntity<Void> deleteCompanyPost(@PathVariable Long userId, @PathVariable Long companyPostId, @AuthenticationPrincipal UserDetails userDetails) {
         companyPostService.deleteCompanyPost(userId, companyPostId);
         return ResponseEntity.noContent().build();
     }
