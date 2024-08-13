ccbf6eefa214bda16da045b7ddf30674d2ff7f75
Lee Haewon <hawon2205@ewhain.net>
2024-08-13 23:49:19 +0900

    feat: 채팅 내용 조회 API 구현

--- src/main/java/com/on/server/domain/chat/dto/ChatListResponseDto.java
+++ src/main/java/com/on/server/domain/chat/dto/ChatListResponseDto.java
@@ -2,6 +2,7 @@ package com.on.server.domain.chat.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Getter
 public class ChatListResponseDto {
     private Long currentUserId;
     private List<chatListDto> chatList;
@@ -16,6 +18,7 @@ public class ChatListResponseDto {
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
+    @Getter
     public static class chatListDto {
         private String message; // 채팅 메시지 내용
         private Long userId; // 채팅 작성자 ID
