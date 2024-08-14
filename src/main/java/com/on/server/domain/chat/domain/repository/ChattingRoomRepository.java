d66e9073dab1a1ea89ac76e6d7a119cf62c89eaf
Lee Haewon <hawon2205@ewhain.net>
2024-08-15 03:28:50 +0900

    fix: merge 충돌 해결

--- src/main/java/com/on/server/domain/chat/domain/repository/ChattingRoomRepository.java
+++ src/main/java/com/on/server/domain/chat/domain/repository/ChattingRoomRepository.java
@@ -1,15 +1,7 @@
 package com.on.server.domain.chat.domain.repository;
 
 import com.on.server.domain.chat.domain.ChattingRoom;
-import com.on.server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long> {
-    List<ChattingRoom> findChattingRoomByChatUserOne(User user);
-
-    List<ChattingRoom> findChattingRoomByChatUserTwo(User user);
-
-    List<ChattingRoom> findChattingRoomByChatUserOneOrChatUserTwo(User chatUserOne, User chatUserTwo);
 }
