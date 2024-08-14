a6180fddf88000de1e5b2290e24bf0ea67425a01
Lee Haewon <hawon2205@ewhain.net>
2024-08-15 03:12:49 +0900

    feat: 채팅방 목록 조회 컨트롤러 변경

--- src/main/java/com/on/server/domain/chat/dto/ChatRequestDto.java
+++ src/main/java/com/on/server/domain/chat/dto/ChatRequestDto.java
@@ -3,11 +3,13 @@ package com.on.server.domain.chat.dto;
 import com.on.server.domain.chat.domain.ChatType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Getter
 public class ChatRequestDto {
     private ChatType chatType;
     private Long receiverId;
--- src/main/java/com/on/server/domain/chat/dto/MarketChatRoomListResponseDto.java
+++ src/main/java/com/on/server/domain/chat/dto/MarketChatRoomListResponseDto.java
@@ -2,6 +2,7 @@ package com.on.server.domain.chat.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -9,13 +10,15 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ChatRoomListResponseDto {
-    private int roomCount; // 채팅방 수
+@Getter
+public class MarketChatRoomListResponseDto {
+    private Integer roomCount; // 채팅방 수
     private List<roomListDto> roomList; // 채팅방 목록
 
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
+    @Getter
     public static class roomListDto {
         private Long roomId;
         private String senderName;
