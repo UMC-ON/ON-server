e0d6c63b2f8555d983f0b01e2094535f7474f911
ri-naa <rina288@ewhain.net>
2024-08-19 12:55:26 +0900

    feat: 채팅방 동행모집 테스트 완료

--- src/main/java/com/on/server/domain/chat/application/ChatService.java
+++ src/main/java/com/on/server/domain/chat/application/ChatService.java
@@ -8,6 +8,7 @@ import com.on.server.domain.chat.domain.repository.ChatRepository;
 import com.on.server.domain.chat.domain.repository.ChattingRoomRepository;
 import com.on.server.domain.chat.domain.repository.SpecialChatRepository;
 import com.on.server.domain.chat.dto.*;
+import com.on.server.domain.companyParticipant.domain.CompanyParticipant;
 import com.on.server.domain.companyParticipant.domain.repository.CompanyParticipantRepository;
 import com.on.server.domain.companyPost.domain.CompanyPost;
 import com.on.server.domain.companyPost.domain.repository.CompanyPostRepository;
@@ -30,6 +31,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
+
+import static com.on.server.domain.companyParticipant.domain.CompanyParticipantStatus.PARTICIPANT;
 
 @Slf4j
 @Service
@@ -271,38 +274,38 @@ public class ChatService {
     }
 
 
-//    @Transactional
-//    public void completeRecruit(User user, Long roomId) {
-//
-//        // 1. companyPost currentRecruitNumber +1 하기
-//        // roomId로 chattingRoom 찾기
-//        ChattingRoom chattingRoom = chattingRoomRepository.findById(roomId)
-//                .orElseThrow(() -> new IllegalArgumentException("채팅방을 찾을 수 없습니다."));
-//
-//        // chattingRoom으로 specialChat 찾기
-//        SpecialChat specialChat = specialChatRepository.findByChattingRoom(chattingRoom);
-//
-//        // specialChat으로 companyPost 찾기
-//        CompanyPost companyPost = companyPostRepository.findBySpecialChat(specialChat);
-//
-//        // 찾은 companyPost currentRecruitNumber +1 하기
-//        Long updateCurrentNumber = companyPost.getCurrentRecruitNumber() + 1 ;
-//        companyPost.updateCurrentNumber(updateCurrentNumber);
-//
-//        companyPostRepository.save(companyPost);
-//
-//
-//        // 2. companyParticipant status -> participant로 바꾸기
-//        User userParticipant = chattingRoom.getChatUserOne();
-//
-//        CompanyParticipant companyParticipant = companyParticipantRepository.findByUser(userParticipant);
-//
-//        // 상태 업데이트
-//        companyParticipant.setCompanyParticipantstatus(PARTICIPANT);
-//
-//        companyParticipantRepository.save(companyParticipant);
-//
-//    }
+    @Transactional
+    public void completeRecruit(User user, Long roomId) {
+
+        // 1. companyPost currentRecruitNumber +1 하기
+        // roomId로 chattingRoom 찾기
+        ChattingRoom chattingRoom = chattingRoomRepository.findById(roomId)
+                .orElseThrow(() -> new IllegalArgumentException("채팅방을 찾을 수 없습니다."));
+
+        // chattingRoom으로 specialChat 찾기
+        SpecialChat specialChat = specialChatRepository.findByChattingRoom(chattingRoom);
+
+        // specialChat에서 companyPost 찾기
+        CompanyPost companyPost = specialChat.getCompanyPost();
+
+        // 찾은 companyPost currentRecruitNumber +1 하기
+        Long updateCurrentNumber = companyPost.getCurrentRecruitNumber() + 1 ;
+        companyPost.updateCurrentNumber(updateCurrentNumber);
+
+        companyPostRepository.save(companyPost);
+
+
+        // 2. companyParticipant status -> participant로 바꾸기
+        User userParticipant = chattingRoom.getChatUserOne();
+
+        CompanyParticipant companyParticipant = companyParticipantRepository.findByUser(userParticipant);
+
+        // 상태 업데이트
+        companyParticipant.setCompanyParticipantstatus(PARTICIPANT);
+
+        companyParticipantRepository.save(companyParticipant);
+
+    }
 
 }
 
