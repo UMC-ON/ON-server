4d644ca7ea322319b9517062d4a0a126e9dfe73f
Lee Haewon <hawon2205@ewhain.net>
2024-08-19 16:23:21 +0900

    feat: 홈 DTO 초기 세팅

---
+++ src/main/java/com/on/server/domain/home/dto/CompanyBoardListResponseDto.java
@@ -0,0 +1,13 @@
+package com.on.server.domain.home.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class CompanyBoardListResponseDto {
+}
