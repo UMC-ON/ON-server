90c7300b86a4ddbaacbae3368543fd08588389ff
chaechaen <leece144@gmail.com>
2024-09-03 21:38:04 +0900

    refactor: UserDetails에서 User 객체를 가져오도록 수정

--- src/main/java/com/on/server/domain/companyPost/presentation/CompanyPostController.java
+++ src/main/java/com/on/server/domain/companyPost/presentation/CompanyPostController.java
@@ -1,11 +1,11 @@
 package com.on.server.domain.companyPost.presentation;
 
 import com.on.server.domain.companyPost.application.CompanyPostService;
-import com.on.server.domain.companyPost.domain.CompanyPost;
 import com.on.server.domain.companyPost.dto.CompanyPostRequestDTO;
 import com.on.server.domain.companyPost.dto.CompanyPostResponseDTO;
 import com.on.server.domain.user.domain.Gender;
 import com.on.server.domain.user.domain.User;
+import com.on.server.global.security.SecurityService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +28,7 @@ import java.util.List;
 public class CompanyPostController {
 
     private final CompanyPostService companyPostService;
+    private final SecurityService securityService;
 
     // 필터링 기능 추가
     @Operation(summary = "필터링된 동행구하기 게시글 조회")
@@ -37,8 +38,7 @@ public class CompanyPostController {
             @RequestParam(required = false) LocalDate startDate,
             @RequestParam(required = false) LocalDate endDate,
             @RequestParam(required = false) Gender gender,
-            @RequestParam(required = false) String country,
-            @AuthenticationPrincipal UserDetails userDetails) {
+            @RequestParam(required = false) String country) {
 
         List<CompanyPostResponseDTO> filteredPosts = companyPostService.getFilteredCompanyPosts(startDate, endDate, gender, country);
         return ResponseEntity.ok(filteredPosts);
@@ -48,18 +48,16 @@ public class CompanyPostController {
     @Operation(summary = "모든 동행구하기 게시글 조회")
     @PreAuthorize("@securityService.isNotTemporaryUser()")
     @GetMapping
-    public ResponseEntity<List<CompanyPostResponseDTO>> getAllCompanyPosts(@AuthenticationPrincipal UserDetails userDetails
-    ) {
+    public ResponseEntity<List<CompanyPostResponseDTO>> getAllCompanyPosts() {
         List<CompanyPostResponseDTO> posts = companyPostService.getAllCompanyPosts();
         return ResponseEntity.ok(posts);
     }
-
 
     // 2. 특정 게시글 조회
     @Operation(summary = "특정 동행구하기 게시글 조회")
     @PreAuthorize("@securityService.isNotTemporaryUser()")
     @GetMapping("/{companyPostId}")
-    public ResponseEntity<List<CompanyPostResponseDTO>> getCompanyPostById(@PathVariable Long companyPostId, @AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<List<CompanyPostResponseDTO>> getCompanyPostById(@PathVariable Long companyPostId) {
         List<CompanyPostResponseDTO> post = companyPostService.getCompanyPostById(companyPostId);
         return ResponseEntity.ok(post);
     }
@@ -73,39 +71,39 @@ public class CompanyPostController {
             @RequestPart(value = "imageFiles", required = false) List<MultipartFile> imageFiles,
             @AuthenticationPrincipal UserDetails userDetails) {
 
-        // 현재 인증된 사용자의 ID를 DTO에 설정
-        if (userDetails instanceof User) {
-            User user = (User) userDetails;
-            requestDTO.setUserId(user.getId());
-        }
+        User user = securityService.getUserByUserDetails(userDetails);
 
-        CompanyPostResponseDTO createdPost = companyPostService.createCompanyPost(requestDTO, imageFiles);
+        CompanyPostResponseDTO createdPost = companyPostService.createCompanyPost(user, requestDTO, imageFiles);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
     }
 
     // 4. 마이페이지에서 자기가 작성한 모든 게시글 조회
     @Operation(summary = "자기가 작성한 모든 동행구하기 게시글 모아보기(조회)")
     @PreAuthorize("@securityService.isNotTemporaryUser()")
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<List<CompanyPostResponseDTO>> getCompanyPostsByUserId(@PathVariable Long userId, @AuthenticationPrincipal UserDetails userDetails) {
-        List<CompanyPostResponseDTO> posts = companyPostService.getCompanyPostsByUserId(userId);
+    @GetMapping("/user")
+    public ResponseEntity<List<CompanyPostResponseDTO>> getCompanyPostsByUser(@AuthenticationPrincipal UserDetails userDetails) {
+        User user = securityService.getUserByUserDetails(userDetails);
+
+        List<CompanyPostResponseDTO> posts = companyPostService.getCompanyPostsByUser(user);
         return ResponseEntity.ok(posts);
     }
 
     // 5. 마이페이지에서 자기가 작성한 특정 게시글 삭제
     @Operation(summary = "자기가 작성한 특정 동행구하기 게시글 삭제")
     @PreAuthorize("@securityService.isNotTemporaryUser()")
-    @DeleteMapping("/user/{userId}/{companyPostId}")
-    public ResponseEntity<Void> deleteCompanyPost(@PathVariable Long userId, @PathVariable Long companyPostId, @AuthenticationPrincipal UserDetails userDetails) {
-        companyPostService.deleteCompanyPost(userId, companyPostId);
-        return ResponseEntity.noContent().build();
+    @DeleteMapping("/user/{companyPostId}")
+    public ResponseEntity<Void> deleteCompanyPost(@PathVariable Long companyPostId, @AuthenticationPrincipal UserDetails userDetails) {
+        User user = securityService.getUserByUserDetails(userDetails);
+
+        companyPostService.deleteCompanyPost(user, companyPostId);
+        return ResponseEntity.ok().build();
     }
 
     // 6. 최신 4개의 동행 구하기 게시글 조회
     @Operation(summary = "최신 4개의 동행구하기글 조회")
     @PreAuthorize("@securityService.isNotTemporaryUser()")
     @GetMapping("/recent")
-    public ResponseEntity<List<CompanyPostResponseDTO>> getRecentCompanyPosts(@AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<List<CompanyPostResponseDTO>> getRecentCompanyPosts() {
         List<CompanyPostResponseDTO> recentPosts = companyPostService.getRecentCompanyPosts();
         return ResponseEntity.ok(recentPosts);
     }
