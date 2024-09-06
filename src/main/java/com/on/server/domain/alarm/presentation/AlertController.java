98667b3d3145a50192255fa8d1eddfe77abb88a7
ri-naa <rina288@ewhain.net>
2024-09-06 16:43:21 +0900

    refactor: 응답 ResponseEntity 로 통일

--- src/main/java/com/on/server/domain/alarm/presentation/AlertController.java
+++ src/main/java/com/on/server/domain/alarm/presentation/AlertController.java
@@ -4,17 +4,19 @@ import com.on.server.domain.alarm.application.AlertService;
 import com.on.server.domain.alarm.dto.AlertListResponseDto;
 import com.on.server.domain.alarm.dto.DeviceTokenRequestDto;
 import com.on.server.domain.user.domain.User;
-import com.on.server.global.common.CommonResponse;
-import com.on.server.global.common.ResponseCode;
 import com.on.server.global.security.SecurityService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @Tag(name = "알림 기록 리스트")
@@ -30,25 +32,35 @@ public class AlertController {
     // 사용자 디바이스 토큰 저장하기
     @PostMapping("/deviceToken")
     @Operation(summary = "사용자 디바이스 토큰 저장하기")
-    @PreAuthorize("@securityService.isActiveAndNotNoneUser() and hasAnyRole('ACTIVE', 'AWAIT', 'TEMPORARY')")
-    public CommonResponse<?> saveDeviceToken(@AuthenticationPrincipal UserDetails userDetails, @RequestBody DeviceTokenRequestDto deviceTokenRequestDto) {
+    @PreAuthorize("@securityService.isNotTemporaryUser() and hasAnyRole('ACTIVE', 'AWAIT', 'TEMPORARY')")
+    public ResponseEntity<?> saveDeviceToken(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody DeviceTokenRequestDto deviceTokenRequestDto
+    ) {
         User user = securityService.getUserByUserDetails(userDetails);
 
         alertService.saveDeviceToken(user, deviceTokenRequestDto);
 
-        return new CommonResponse<>(ResponseCode.SUCCESS);
+        return ResponseEntity.ok().build();
     }
 
     // 알림 리스트 조회
     @GetMapping("/list")
     @Operation(summary = "사용자 알림 리스트 조회하기")
-    @PreAuthorize("@securityService.isActiveAndNotNoneUser() and hasAnyRole('ACTIVE', 'AWAIT', 'TEMPORARY')")
-    public CommonResponse<?> getAlertList(@AuthenticationPrincipal UserDetails userDetails) {
+    @PreAuthorize("@securityService.isNotTemporaryUser() and hasAnyRole('ACTIVE', 'AWAIT', 'TEMPORARY')")
+    public ResponseEntity<?> getAlertList(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @ParameterObject Pageable pageable
+    ) {
         User user = securityService.getUserByUserDetails(userDetails);
 
-        List<AlertListResponseDto> alertResponseDtoList = alertService.getAlertList(user);
+        List<AlertListResponseDto> alertResponseDtoList = alertService.getAlertList(user, pageable);
 
-        return new CommonResponse<>(ResponseCode.SUCCESS, alertResponseDtoList);
+        return ResponseEntity.ok(alertResponseDtoList);
+
+//        Long id = alertResponseDtoList.get(0).getAlertConnectId();
+//        //ResponseEntity.created() 으로 API 만들어서 보내주기
+//        return ResponseEntity.created(URI.create("/users/" + id)).build();
     }
 
 }
