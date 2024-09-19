f827a41b203c969474705b8c1e3be86b51515c49
dustbox <sanggae4133@naver.com>
2024-09-19 15:27:34 +0900

    refactor: controller ResponseEntity로 교체

--- src/main/java/com/on/server/domain/user/presentation/UserController.java
+++ src/main/java/com/on/server/domain/user/presentation/UserController.java
@@ -15,6 +15,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.validator.constraints.URL;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -34,7 +36,7 @@ public class UserController {
     // 로그인
     @Operation(summary = "로그인")
     @PostMapping("/sign-in")
-    public CommonResponse<JwtToken> signIn(
+    public ResponseEntity<JwtToken> signIn(
             @RequestBody SignInRequestDto signInRequestDto
     ) {
         log.info("sign-in");
@@ -43,87 +45,88 @@ public class UserController {
         JwtToken jwtToken = userService.signIn(email, password);
         log.info("request loginId = {}, password = {}", email, password);
         log.info("jwtToken accessToken = {}, refreshToken = {}", jwtToken.getAccessToken(), jwtToken.getRefreshToken());
-        return CommonResponse.ok(jwtToken);
+
+        return ResponseEntity.ok(jwtToken);
     }
 
     // 회원가입
     @Operation(summary = "회원가입")
     @PostMapping("/sign-up")
-    public CommonResponse<Void> signUp(
+    public ResponseEntity<Void> signUp(
             @RequestBody SignUpRequestDto signUpRequestDto
     ) {
         userService.signUp(signUpRequestDto);
-        return CommonResponse.success();
+        return ResponseEntity.ok().build();
     }
 
     // 이메일 중복 체크
     @Operation(summary = "이메일 중복 체크")
     @PostMapping("/duplicate_check/email")
-    public CommonResponse<Boolean> isDuplicateEmail(
+    public ResponseEntity<Boolean> isDuplicateEmail(
             @RequestBody String email
     ) {
-        return CommonResponse.ok(userService.isDuplicateEmail(email));
+        return ResponseEntity.ok(userService.isDuplicateEmail(email));
     }
 
     // 닉네임 중복 체크
     @Operation(summary = "닉네임 중복 체크")
     @PostMapping("/duplicate_check/nickname")
-    public CommonResponse<Boolean> isDuplicateNickname(
+    public ResponseEntity<Boolean> isDuplicateNickname(
             @RequestBody String nickname
     ) {
-        return CommonResponse.ok(userService.isDuplicateNickname(nickname));
+        return ResponseEntity.ok(userService.isDuplicateNickname(nickname));
     }
 
     // 현재 사용자 UserStatus 조회
     @Operation(summary = "현재 사용자 UserStatus 조회")
     @GetMapping("/current/status")
-    public CommonResponse<UserStatus> getCurrentUserStatus(
+    public ResponseEntity<UserStatus> getCurrentUserStatus(
             @AuthenticationPrincipal UserDetails userDetails
     ) {
-        return CommonResponse.ok(userService.getUserStatusByEmail(userDetails.getUsername()));
+        return ResponseEntity.ok(userService.getUserStatusByEmail(userDetails.getUsername()));
     }
 
     // 현재 사용자 정보 조회
     @Operation(summary = "현재 사용자 정보 조회")
     @GetMapping("/current/info")
-    public CommonResponse<UserInfoResponseDto> getCurrentUser(
+    public ResponseEntity<UserInfoResponseDto> getCurrentUser(
             @AuthenticationPrincipal UserDetails userDetails
     ) {
-        return CommonResponse.ok(userService.getUserInfoByEmail(userDetails.getUsername()));
+        return ResponseEntity.ok(userService.getUserInfoByEmail(userDetails.getUsername()));
     }
 
     // 현재 사용자 닉네임 수정
     @Operation(summary = "현재 사용자 닉네임 수정")
     @PutMapping("/current/update/nickname")
     @PreAuthorize("@securityService.isNotTemporaryUser()")
-    public CommonResponse<Void> updateCurrentUserNickname(
+    public ResponseEntity<Void> updateCurrentUserNickname(
             @AuthenticationPrincipal UserDetails userDetails,
             @RequestBody String nickname
     ) {
         userService.updateUserNickName(securityService.getUserByUserDetails(userDetails), nickname);
-        return CommonResponse.success();
+        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "현재 사용자 교환/방문교 URL 수정")
     @PutMapping("/current/update/univ_url")
     @PreAuthorize("@securityService.isNotTemporaryUser()")
-    public CommonResponse<Void> updateCurrentUserUniversityUrl(
+    public ResponseEntity<Void> updateCurrentUserUniversityUrl(
             @AuthenticationPrincipal UserDetails userDetails,
             @RequestBody @URL(message = "올바른 URL 형식이 아닙니다.") String universityUrl
     ) {
         userService.updateUserUniversityUrl(securityService.getUserByUserDetails(userDetails), universityUrl);
-        return CommonResponse.success();
+        return ResponseEntity.ok().build();
     }
 
     // 회원 탈퇴
     @Operation(summary = "회원 탈퇴")
     @DeleteMapping("/delete")
     @PreAuthorize("@securityService.isNotTemporaryUser()")
-    public CommonResponse<Void> deleteUser(
+    public ResponseEntity<Void> deleteUser(
             @AuthenticationPrincipal UserDetails userDetails
     ) {
         userService.deleteUser(securityService.getUserByUserDetails(userDetails));
-        return CommonResponse.success();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 
@@ -136,12 +139,12 @@ public class UserController {
     @PostMapping("/test")
     //
     @PreAuthorize("@securityService.isNotTemporaryUser() or hasAnyRole('ADMIN', 'ACTIVE', 'AWAIT', 'DENIED', 'NON_CERTIFIED')")
-    public CommonResponse<User> test(
+    public ResponseEntity<User> test(
             @AuthenticationPrincipal UserDetails userDetails
             ) {
         User user = securityService.getUserByUserDetails(userDetails);
 
-        return CommonResponse.ok(userService.test(user));
+        return ResponseEntity.ok(userService.test(user));
     }
 
 }
