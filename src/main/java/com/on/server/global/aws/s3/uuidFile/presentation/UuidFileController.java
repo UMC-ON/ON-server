815964d5059678986723fdd2660c4cefe770a62c
dustbox <sanggae4133@naver.com>
2024-09-20 02:24:37 +0900

    fix: uuidfile controller 변경

--- src/main/java/com/on/server/global/aws/s3/uuidFile/presentation/UuidFileController.java
+++ src/main/java/com/on/server/global/aws/s3/uuidFile/presentation/UuidFileController.java
@@ -3,11 +3,11 @@ package com.on.server.global.aws.s3.uuidFile.presentation;
 import com.on.server.global.aws.s3.uuidFile.application.UuidFileService;
 import com.on.server.global.aws.s3.uuidFile.domain.FilePath;
 import com.on.server.global.aws.s3.uuidFile.domain.UuidFile;
-import com.on.server.global.common.CommonResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,28 +21,28 @@ public class UuidFileController {
 
     @Operation(summary = "UuidFile 상세 조회, test 용도")
     @GetMapping("/{id}")
-    public CommonResponse<UuidFile> getUuidFile(
-            @RequestParam(name = "id") Long id
+    public ResponseEntity<UuidFile> getUuidFile(
+            @PathVariable(name = "id") Long id
     ) {
-        return CommonResponse.ok(uuidFileService.findUuidFileById(id));
+        return ResponseEntity.ok(uuidFileService.findUuidFileById(id));
     }
 
     @Operation(summary = "UuidFile 업로드, test 용도")
     @PostMapping(value = "/upload/{filePath}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public CommonResponse<UuidFile> uploadFile(
-            @RequestParam(name = "filePath") FilePath filePath,
+    public ResponseEntity<UuidFile> uploadFile(
+            @PathVariable(name = "filePath") FilePath filePath,
             @RequestPart(name = "file") MultipartFile file
     ) {
-        return CommonResponse.ok(uuidFileService.saveFile(file, filePath));
+        return ResponseEntity.ok(uuidFileService.saveFile(file, filePath));
     }
 
     @Operation(summary = "UuidFile 삭제, test 용도")
     @DeleteMapping("/{id}")
-    public CommonResponse<Void> deleteFile(
-            @RequestParam(name = "id") Long id
+    public ResponseEntity<Void> deleteFile(
+            @PathVariable(name = "id") Long id
     ) {
         uuidFileService.deleteFile(uuidFileService.findUuidFileById(id));
 
-        return CommonResponse.success();
+        return ResponseEntity.ok().build();
     }
 }
