b2de6a53d86856b354fbfe50d0a4fea48735f789
dustbox <sanggae4133@naver.com>
2024-09-19 21:13:56 +0900

    fix: disaptch-certify controller layer ResponseEntity로 변경

--- src/main/java/com/on/server/domain/dispatchCertify/persentation/DispatchCertifyController.java
+++ src/main/java/com/on/server/domain/dispatchCertify/persentation/DispatchCertifyController.java
@@ -13,6 +13,7 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -32,90 +33,90 @@ public class DispatchCertifyController {
 
     @Operation(summary = "교환/파견교 인증 신청")
     @PostMapping(value = "/apply", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public CommonResponse<Void> applyDispatchCertify(
+    public ResponseEntity<Void> applyDispatchCertify(
             @AuthenticationPrincipal UserDetails userDetails,
             @RequestPart(value = "dispatchCertifyApplyRequestDto") DispatchCertifyApplyRequestDto dispatchCertifyApplyRequestDto,
             @RequestPart(value = "fileList") List<MultipartFile> fileList
     ) {
         dispatchCertifyService.applyDispatchCertify(securityService.getUserByUserDetails(userDetails), dispatchCertifyApplyRequestDto, fileList);
-        return CommonResponse.success();
+        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "교환/파견교 미정 확정")
     @PostMapping("/non_certified")
-    public CommonResponse<Void> nonCertifiedDispatchCertify(
+    public ResponseEntity<Void> nonCertifiedDispatchCertify(
             @AuthenticationPrincipal UserDetails userDetails
     ) {
         dispatchCertifyService.setNonCertified(securityService.getUserByUserDetails(userDetails));
-        return CommonResponse.success();
+        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "교환/파견교 인증 정보 상세 조회(관리자만 사용 가능)")
     @GetMapping("/info/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public CommonResponse<DispatchCertifyInfoResponseDto> getDispatchCertifyInfo(
+    public ResponseEntity<DispatchCertifyInfoResponseDto> getDispatchCertifyInfo(
             @PathVariable(value = "id") Long id
     ) {
-        return CommonResponse.ok(dispatchCertifyService.getDispatchCertifyInfo(id));
+        return ResponseEntity.ok(dispatchCertifyService.getDispatchCertifyInfo(id));
     }
 
     @Operation(summary = "본인의 교환/파견교 인증 정보 페이지 조회")
     @PostMapping("/info/current")
-    public CommonResponse<Page<DispatchCertifyInfoResponseDto>> getCurrentDispatchCertifyInfoPageByCurrentUser(
+    public ResponseEntity<Page<DispatchCertifyInfoResponseDto>> getCurrentDispatchCertifyInfoPageByCurrentUser(
             @AuthenticationPrincipal UserDetails userDetails,
             @ParameterObject Pageable pageable
     ) {
-        return CommonResponse.ok(dispatchCertifyService.getDispatchCertifyInfoPageByUser(securityService.getUserByUserDetails(userDetails), pageable));
+        return ResponseEntity.ok(dispatchCertifyService.getDispatchCertifyInfoPageByUser(securityService.getUserByUserDetails(userDetails), pageable));
     }
 
     @Operation(summary = "특정 사용자의 교환/파견교 인증 정보 페이지 조회(관리자만 사용 가능)")
     @PostMapping("/info/target-user/{targetUserId}")
     @PreAuthorize("hasRole('ADMIN')")
-    public CommonResponse<Page<DispatchCertifyInfoResponseDto>> getDispatchCertifyInfoPageByUser(
+    public ResponseEntity<Page<DispatchCertifyInfoResponseDto>> getDispatchCertifyInfoPageByUser(
             @PathVariable(value = "targetUserId") Long targetUserId,
             @ParameterObject Pageable pageable
     ) {
-        return CommonResponse.ok(dispatchCertifyService.getDispatchCertifyInfoPageByUser(targetUserId, pageable));
+        return ResponseEntity.ok(dispatchCertifyService.getDispatchCertifyInfoPageByUser(targetUserId, pageable));
     }
 
     @Operation(summary = "특정 상태의 교환/파견교 인증 정보 페이지 조회(관리자만 사용 가능)")
     @PostMapping("/info/{permitStatus}")
     @PreAuthorize("hasRole('ADMIN')")
-    public CommonResponse<Page<DispatchCertifyInfoResponseDto>> getDispatchCertifyInfoPageByPermitStatus(
+    public ResponseEntity<Page<DispatchCertifyInfoResponseDto>> getDispatchCertifyInfoPageByPermitStatus(
             @PathVariable(value = "permitStatus") PermitStatus permitStatus,
             @ParameterObject Pageable pageable
     ) {
-        return CommonResponse.ok(dispatchCertifyService.getDispatchCertifyInfoPageByPermitStatus(permitStatus, pageable));
+        return ResponseEntity.ok(dispatchCertifyService.getDispatchCertifyInfoPageByPermitStatus(permitStatus, pageable));
     }
 
     @Operation(summary = "교환/파견교 인증 상태 변경(관리자만 사용 가능)")
     @PutMapping("/change-status/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public CommonResponse<Void> changePermitStatus(
+    public ResponseEntity<Void> changePermitStatus(
             @PathVariable(value = "id") Long id,
             @RequestBody PermitStatus permitStatus
     ) {
         dispatchCertifyService.changePermitStatus(id, permitStatus);
-        return CommonResponse.success();
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public CommonResponse<Void> deleteDispatchCertify(
+    public ResponseEntity<Void> deleteDispatchCertify(
             @PathVariable(value = "id") Long id
     ) {
         dispatchCertifyService.deleteDispatchCertify(id);
-        return CommonResponse.success();
+        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "본인의 교환/파견교 인증 정보 삭제")
     @DeleteMapping("/delete/self/{id}")
-    public CommonResponse<Void> deleteSelfDispatchCertify(
+    public ResponseEntity<Void> deleteSelfDispatchCertify(
             @AuthenticationPrincipal UserDetails userDetails,
             @PathVariable(value = "id") Long id
     ) {
         dispatchCertifyService.deleteDispatchCertify(securityService.getUserByUserDetails(userDetails), id);
-        return CommonResponse.success();
+        return ResponseEntity.ok().build();
     }
 
 }
