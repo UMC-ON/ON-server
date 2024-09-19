adf353455f7557995161255b84e28df530581beb
dustbox <sanggae4133@naver.com>
2024-09-19 21:05:13 +0900

    fix: 회원 탈퇴 로직 수정

--- src/main/java/com/on/server/global/redis/RedisUtils.java
+++ src/main/java/com/on/server/global/redis/RedisUtils.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
