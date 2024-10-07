4e8d54b9491409b1e5a539d6afcaea607aaacaaa
Lee Haewon <hawon2205@ewhain.net>
2024-10-07 19:35:24 +0900

    fix: 동행 채팅방 목록 null인 경우 리턴값 수정

--- src/main/java/com/on/server/domain/chat/application/ChatService.java
+++ src/main/java/com/on/server/domain/chat/application/ChatService.java
@@ -80,7 +80,7 @@ public class ChatService {
                     String senderName = Objects.equals(user.getId(), chatUserOne.getId()) ? chatUserTwo.getNickname() : chatUserOne.getNickname();
 
                     String chatContents = (chat != null) ? chat.getContents() : null;
-                    String lastChatTime = (chat != null) ? formatLastChatTime(chat.getCreatedAt()) : null;
+                    String lastChatTime = (chat != null) ? formatLastChatTime(chat.getCreatedAt()) : formatLastChatTime(chattingRoom.getCreatedAt());
 
                     return new CompanyRoomDto(
                             chattingRoom.getId(),
